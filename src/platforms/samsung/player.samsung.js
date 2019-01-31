8d6d643853517a8f846cdce21754fe63b022488b
Igor Suvorov <suvorov.igor.90@gmail.com>
2019-01-31 15:36:25 +0200

    Update player.samsung.js

--- src/platforms/samsung/player.samsung.js
+++ src/platforms/samsung/player.samsung.js
@@ -24,6 +24,8 @@ SB.readyForPlatform('samsung', function () {
                     return self[method](args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
                 case 8:
                     return self[method](args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
+                case 9:
+                    return self[method](args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
 
             }
         } catch (e) {
